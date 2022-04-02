9c5fd1260447c07857d146e2a9adb0876c1a99c7
BrayanMa <mariebrayan724@gmail.com>
2022-04-02 11:39:31 +0200

    Objectif 2 - Points génériques

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 
 int main(int argc, char** argv)
 {
+    /*Point<float, 3> p1;
+    Point<float, 3> p2;
+    auto p3 = p1 + p2;
+    p1 += p2;
+    p1 *= 3;*/
     ContextInitializer context_initializer = ContextInitializer {argc , argv};
     TowerSimulation simulation { context_initializer };
     simulation.launch();
